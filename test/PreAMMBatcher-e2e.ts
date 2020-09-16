82ade379796c05c8a00ad05c09d5a7ca77409758
josojo <josojo@hotmail.de>
2020-09-16 11:29:58 +0200

    refactoring test suite

--- test/PreAMMBatcher-e2e.ts
+++ test/PreAMMBatcher-e2e.ts
@@ -1,5 +1,5 @@
 import {use, expect} from 'chai';
-import {Contract, utils} from 'ethers';
+import {Contract, Wallet} from 'ethers';
 import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
 import PreAMMBatcher from '../build/PreAMMBatcher.json';
 import UniswapV2Pair from '../node_modules/@uniswap/v2-core/build/UniswapV2Pair.json';
@@ -8,6 +8,8 @@ import UniswapV2Factory from '../node_modules/@uniswap/v2-core/build/UniswapV2Fa
 import ERC20 from '../build/ERC20Mintable.json';
 import {Order} from '../src/js/orders.spec';
 import BN from 'bn.js';
+import {baseTestInput, generateTestCase} from './resources/index';
+import {TestCase} from './resources/models';
 
 use(solidity);
 
@@ -17,13 +19,21 @@ async function asyncForEach(array: Order[], callback: any) {
   }
 }
 
-const setupOrder = async (orders: Order[], batcher: Contract) => {
+const setupOrders = async (orders: Order[], batcher: Contract) => {
   await asyncForEach(orders, async (order: Order) => {
     await order.sellToken.mint(order.wallet.address, order.sellAmount);
     await order.sellToken.connect(order.wallet).approve(batcher.address, order.sellAmount);
   });
 };
-
+const fundUniswap = async (testCase: TestCase, walletDeployer: Wallet, uniswapPair: Contract) => {
+  const token0 = testCase.sellOrdersToken0[0].sellToken;
+  const token1 = testCase.sellOrdersToken0[0].buyToken;
+  await token0.mint(walletDeployer.address, testCase.fundingAMMToken0);
+  await token1.mint(walletDeployer.address, testCase.fundingAMMToken1);
+  await token0.transfer(uniswapPair.address, testCase.fundingAMMToken0);
+  await token1.transfer(uniswapPair.address, testCase.fundingAMMToken0);
+  await uniswapPair.mint(walletDeployer.address, {gasLimit: 500000});
+};
 describe('PreAMMBatcher-e2e', () => {
   const [walletDeployer, walletTrader1, walletTrader2] = new MockProvider().getWallets();
   let batcher: Contract;
@@ -32,6 +42,7 @@ describe('PreAMMBatcher-e2e', () => {
   let uniswapPair: Contract;
   let uniswapFactory: Contract;
   let uniswapPairAddress: string;
+  let basicTestCase: TestCase;
 
   beforeEach(async () => {
     token0 = await deployContract(walletDeployer, ERC20, ['token0', '18']);
@@ -42,27 +53,21 @@ describe('PreAMMBatcher-e2e', () => {
     uniswapPair = await deployContract(walletDeployer, UniswapV2Pair);
     uniswapPair = await uniswapPair.attach(uniswapPairAddress);
     batcher = await deployContract(walletDeployer, PreAMMBatcher, [uniswapFactory.address]);
-
-    await token0.mint(walletDeployer.address, utils.parseEther('10'));
-    await token1.mint(walletDeployer.address, utils.parseEther('10'));
-    await token0.transfer(uniswapPair.address, utils.parseEther('10'));
-    await token1.transfer(uniswapPair.address, utils.parseEther('10'));
-    await uniswapPair.mint(walletDeployer.address, {gasLimit: 500000});
-    expect((await uniswapPair.getReserves())[0]).to.equal(utils.parseEther('10'));
   });
 
   it('pre-batches two simple orders and settles left-overs to uniswap', async () => {
-    const sellToken0Order = new Order(utils.parseEther('1'),
-      utils.parseEther('0.9'), token0, token1, walletTrader1, 1);
-    const sellToken1Order = new Order(utils.parseEther('0.9'),
-      utils.parseEther('0.90111'), token1, token0, walletTrader2, 1);
+    basicTestCase = generateTestCase(baseTestInput(token0, token1, [walletTrader1], [walletTrader2]));
+    await fundUniswap(basicTestCase, walletDeployer, uniswapPair);
 
-    await setupOrder([sellToken0Order, sellToken1Order], batcher);
+    await setupOrders(basicTestCase.sellOrdersToken0.concat(basicTestCase.sellOrdersToken1), batcher);
 
-    await expect(batcher.batchTrade(sellToken0Order.encode(), sellToken1Order.encode(), {gasLimit: 6000000}))
+    await expect(batcher.batchTrade(basicTestCase.getSellOrdersToken0Encoded(),
+      basicTestCase.getSellOrdersToken1Encoded(), {gasLimit: 6000000}))
       .to.emit(batcher, 'BatchSettlement')
-      .withArgs(token0.address, token1.address, '9916608715780969175', '10084092542732199005');
+      .withArgs(token0.address, token1.address,
+        basicTestCase.solution.clearingPrice.denominator, basicTestCase.solution.clearingPrice.numerator);
 
+    // reserves do not agree exactly with clearing price, as calculations do not consider uniswap fees
     expect((await uniswapPair.getReserves())[0]).to.equal('10084799698306515679');
     expect((await uniswapPair.getReserves())[1]).to.equal('9916858889633626266');
     console.log('auction clearing price:',
---
+++ test/resources/index.ts
@@ -0,0 +1,72 @@
+import {Fraction, TestCaseInput, TestCase} from './models';
+import {BigNumber, Contract, Wallet, utils} from 'ethers';
+import {Order} from '../../src/js/orders.spec';
+
+export const baseTestInput = function (token0: Contract, token1: Contract, tradersToken0: Wallet[],
+  tradersToken1: Wallet[]):
+  TestCaseInput {
+  return {
+    fundingAMMToken0: utils.parseEther('10'),
+    fundingAMMToken1: utils.parseEther('10'),
+    sellOrdersToken0: [new Order(utils.parseEther('1'),
+      utils.parseEther('0.9'), token0, token1, tradersToken0[0], 1)],
+    sellOrdersToken1: [new Order(utils.parseEther('0.9'),
+      utils.parseEther('0.90111'), token1, token0, tradersToken1[0], 1)]
+  };
+};
+
+export const generateTestCase = function (testCaseInput: TestCaseInput): TestCase {
+  let sumSellDemandToken0 = BigNumber.from(0);
+  testCaseInput.sellOrdersToken0.forEach(order => {
+    sumSellDemandToken0 = sumSellDemandToken0.add(order.sellAmount);
+  });
+
+  let sumSellAmountToken1 = BigNumber.from(0);
+  testCaseInput.sellOrdersToken1.forEach(order => {
+    sumSellAmountToken1 = sumSellAmountToken1.add(order.sellAmount);
+  });
+
+  const uniswapK = testCaseInput.fundingAMMToken0.mul(testCaseInput.fundingAMMToken1);
+  const p = uniswapK.div(
+    BigNumber.from(2).mul(testCaseInput.fundingAMMToken1.add(sumSellAmountToken1))
+  );
+
+  const newFundingAMMToken0 = p.add(
+    SoliditySqrt(
+      p.mul(p).add(
+        uniswapK.mul(sumSellDemandToken0).div(
+          testCaseInput.fundingAMMToken1.add(sumSellAmountToken1)
+        )
+      )
+    )
+  );
+
+  const newFundingAMMToken1 = uniswapK.div(newFundingAMMToken0);
+  const clearingPrice: Fraction = {
+    numerator: newFundingAMMToken0,
+    denominator: newFundingAMMToken1
+  };
+  const settledAmountsSellOrderToken0 = testCaseInput.sellOrdersToken0.map(order => order.sellAmount);
+  const settledAmountsSellOrderToken1 = testCaseInput.sellOrdersToken1.map(order => order.sellAmount);
+  return new TestCase(
+    testCaseInput.fundingAMMToken0, testCaseInput.fundingAMMToken1, testCaseInput.sellOrdersToken0,
+    testCaseInput.sellOrdersToken1, {clearingPrice: clearingPrice,
+      settledAmountsSellOrderToken0: settledAmountsSellOrderToken0,
+      settledAmountsSellOrderToken1: settledAmountsSellOrderToken1}
+  );
+};
+
+export const SoliditySqrt = function (y: BigNumber): BigNumber {
+  let z = BigNumber.from(0);
+  if (y.gt(3)) {
+    z = y;
+    let x = y.div(2).add(1);
+    while (x.lt(z)) {
+      z = x;
+      x = (y.div(x).add(x)).div(2);
+    }
+  } else if (!y.isZero()) {
+    z = BigNumber.from(1);
+  }
+  return z;
+};
