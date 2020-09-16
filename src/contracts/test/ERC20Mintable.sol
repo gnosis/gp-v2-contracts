0b3de8e6c4a94f3aae50028c8adcadfc71b367aa
josojo <josojo@hotmail.de>
2020-09-16 08:21:08 +0200

    using the right bigNumber format for orders

---
+++ src/contracts/test/ERC20Mintable.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Mintable is ERC20 {
+    constructor(string memory name, string memory symbol)
+        public
+        ERC20(name, symbol)
+    {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
