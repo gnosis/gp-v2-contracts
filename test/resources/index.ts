d2907b942e57bffbdd408e93dccf8f2d0b3692f3
josojo <josojo@hotmail.de>
2020-09-17 09:17:42 +0200

    Kill or fill approach (JS tests only)

--- test/resources/index.ts
+++ test/resources/index.ts
@@ -1,38 +1,15 @@
-import {Fraction, TestCaseInput, TestCase} from './models';
-import {BigNumber, Contract, Wallet, utils} from 'ethers';
-import {Order} from '../../src/js/orders.spec';
+import {Fraction, TestCaseInput, Solution, TestCase} from './models';
+import {BigNumber} from 'ethers';
 
-export const baseTestInput = function (token0: Contract, token1: Contract, tradersToken0: Wallet[],
-  tradersToken1: Wallet[]):
-  TestCaseInput {
-  return {
-    fundingAMMToken0: utils.parseEther('10'),
-    fundingAMMToken1: utils.parseEther('10'),
-    sellOrdersToken0: [new Order(utils.parseEther('1'),
-      utils.parseEther('0.9'), token0, token1, tradersToken0[0], 1)],
-    sellOrdersToken1: [new Order(utils.parseEther('0.9'),
-      utils.parseEther('0.90111'), token1, token0, tradersToken1[0], 1)]
-  };
+export const generateTestCase = function (testCaseInput: TestCaseInput, debug = false): TestCase {
+  return new TestCase(testCaseInput.fundingAMMToken0, testCaseInput.fundingAMMToken1,
+    testCaseInput.sellOrdersToken0, testCaseInput.sellOrdersToken1, solveTestCase(testCaseInput, debug));
 };
 
-export const fourOrderTestInput = function (token0: Contract, token1: Contract, tradersToken0: Wallet[],
-  tradersToken1: Wallet[]):
-  TestCaseInput {
-  return {
-    fundingAMMToken0: utils.parseEther('10'),
-    fundingAMMToken1: utils.parseEther('10'),
-    sellOrdersToken0: [new Order(utils.parseEther('1'),
-      utils.parseEther('0.9'), token0, token1, tradersToken0[0], 1),
-    new Order(utils.parseEther('0.5'),
-      utils.parseEther('0.45'), token0, token1, tradersToken0[1], 2)],
-    sellOrdersToken1: [new Order(utils.parseEther('0.9'),
-      utils.parseEther('0.90111'), token1, token0, tradersToken1[0], 1),
-    new Order(utils.parseEther('0.45'),
-      utils.parseEther('0.45'), token1, token0, tradersToken1[1], 1)]
-  };
-};
+export const solveTestCase = function (testCaseInput: TestCaseInput, debug = false): Solution {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const log = debug ? (...a: any) => console.log(...a) : () => {};
 
-export const generateTestCase = function (testCaseInput: TestCaseInput): TestCase {
   let sumSellDemandToken0 = BigNumber.from(0);
   testCaseInput.sellOrdersToken0.forEach(order => {
     sumSellDemandToken0 = sumSellDemandToken0.add(order.sellAmount);
@@ -42,7 +19,29 @@ export const generateTestCase = function (testCaseInput: TestCaseInput): TestCas
   testCaseInput.sellOrdersToken1.forEach(order => {
     sumSellAmountToken1 = sumSellAmountToken1.add(order.sellAmount);
   });
+  if (sumSellDemandToken0.isZero() || sumSellAmountToken1.isZero()) {
+    log('no solution found');
+    return ({clearingPrice: {numerator: BigNumber.from('0'), denominator: BigNumber.from('0')},
+      sellOrdersToken0: [],
+      sellOrdersToken1: []
+    });
+  }
+  const highestSellOrderToken0 = testCaseInput.sellOrdersToken0[testCaseInput.sellOrdersToken0.length - 1];
+  const highestSellOrderToken1 = testCaseInput.sellOrdersToken1[testCaseInput.sellOrdersToken1.length - 1];
+  // We will always assume that we have a unsettlable amount in token0. If not, we change order of
+  log('Total selling demand token0', sumSellDemandToken0.toString());
+  log('Total selling demand token1', sumSellAmountToken1.toString());
+  log('Highest bid selling OrderToken0', highestSellOrderToken0.sellAmount.toString());
+  log('Highest bid selling OrderToken1', highestSellOrderToken0.buyAmount.toString());
+  if (sumSellDemandToken0.mul(highestSellOrderToken1.sellAmount)
+    .lt(sumSellAmountToken1.mul(highestSellOrderToken1.buyAmount))) {
+    log('switching token0 and token1');
 
+    return solveTestCase({fundingAMMToken0: testCaseInput.fundingAMMToken1,
+      fundingAMMToken1: testCaseInput.fundingAMMToken0,
+      sellOrdersToken0: testCaseInput.sellOrdersToken1,
+      sellOrdersToken1: testCaseInput.sellOrdersToken0}, debug);
+  }
   const uniswapK = testCaseInput.fundingAMMToken0.mul(testCaseInput.fundingAMMToken1);
   const p = uniswapK.div(
     BigNumber.from(2).mul(testCaseInput.fundingAMMToken1.add(sumSellAmountToken1))
@@ -63,14 +62,39 @@ export const generateTestCase = function (testCaseInput: TestCaseInput): TestCas
     numerator: newFundingAMMToken0,
     denominator: newFundingAMMToken1
   };
-  const settledAmountsSellOrderToken0 = testCaseInput.sellOrdersToken0.map(order => order.sellAmount);
-  const settledAmountsSellOrderToken1 = testCaseInput.sellOrdersToken1.map(order => order.sellAmount);
-  return new TestCase(
-    testCaseInput.fundingAMMToken0, testCaseInput.fundingAMMToken1, testCaseInput.sellOrdersToken0,
-    testCaseInput.sellOrdersToken1, {clearingPrice: clearingPrice,
-      settledAmountsSellOrderToken0: settledAmountsSellOrderToken0,
-      settledAmountsSellOrderToken1: settledAmountsSellOrderToken1}
-  );
+  log('new clearing price', clearingPrice.denominator.toString(), '/', clearingPrice.numerator.toString(),
+    '=', clearingPrice.denominator.mul(1000).div(clearingPrice.numerator).toNumber() / 1000);
+  if (highestSellOrderToken0.sellAmount.mul(clearingPrice.denominator)
+    .lte(highestSellOrderToken0.buyAmount.mul(clearingPrice.numerator))) {
+    // In this case the clearing price of the order selling token0 with the highest price is violated.
+    // We remove this bid and try to solve again
+
+    // Actually, the bid's sellAmount could also just be reduced, if uniswap prices(reserve0/reserve1)
+    // is smaller than bid-price. We leave this optimzation for later and go with the principle fill or kill
+    console.log(`popping sellOrdersToken0, due to a its limit price of
+     ${highestSellOrderToken0.sellAmount} / ${highestSellOrderToken0.buyAmount}`);
+    testCaseInput.sellOrdersToken0.pop();
+    return solveTestCase({fundingAMMToken0: testCaseInput.fundingAMMToken0,
+      fundingAMMToken1: testCaseInput.fundingAMMToken1,
+      sellOrdersToken0: testCaseInput.sellOrdersToken0,
+      sellOrdersToken1: testCaseInput.sellOrdersToken1}, debug);
+  } else if (clearingPrice.numerator.mul(highestSellOrderToken1.sellAmount)
+    .lte(clearingPrice.denominator.mul(highestSellOrderToken1.buyAmount))) {
+    // In this case the clearing price of the order selling token1 with the highest price is violated.
+    // We remove this bid and try to solve again
+    console.log(`popping sellOrdersToken1, due to a its limit price of
+     ${highestSellOrderToken1.buyAmount} / ${highestSellOrderToken1.sellAmount}`);
+    testCaseInput.sellOrdersToken1.pop();
+    return solveTestCase({fundingAMMToken0: testCaseInput.fundingAMMToken0,
+      fundingAMMToken1: testCaseInput.fundingAMMToken1,
+      sellOrdersToken0: testCaseInput.sellOrdersToken0,
+      sellOrdersToken1: testCaseInput.sellOrdersToken1}, debug);
+  }
+  // No price violation, we found a solution:
+  return {clearingPrice: clearingPrice,
+    sellOrdersToken0: testCaseInput.sellOrdersToken0,
+    sellOrdersToken1: testCaseInput.sellOrdersToken1
+  };
 };
 
 export const SoliditySqrt = function (y: BigNumber): BigNumber {
---
+++ test/resources/testExamples.ts
@@ -0,0 +1,110 @@
+import {TestCaseInput} from './models';
+import {Contract, Wallet, utils} from 'ethers';
+import {Order} from '../../src/js/orders.spec';
+
+export const baseTestInput = function (token0: Contract, token1: Contract, tradersToken0: Wallet[],
+  tradersToken1: Wallet[]):
+  TestCaseInput {
+  return {
+    fundingAMMToken0: utils.parseEther('10'),
+    fundingAMMToken1: utils.parseEther('10'),
+    sellOrdersToken0: [new Order(utils.parseEther('1'),
+      utils.parseEther('0.9'), token0, token1, tradersToken0[0], 1)],
+    sellOrdersToken1: [new Order(utils.parseEther('0.9'),
+      utils.parseEther('0.90111'), token1, token0, tradersToken1[0], 1)]
+  };
+};
+
+export const fourOrderTestInput = function (token0: Contract, token1: Contract, tradersToken0: Wallet[],
+  tradersToken1: Wallet[]):
+  TestCaseInput {
+  return {
+    fundingAMMToken0: utils.parseEther('10'),
+    fundingAMMToken1: utils.parseEther('10'),
+    sellOrdersToken0: [new Order(utils.parseEther('1'),
+      utils.parseEther('0.9'), token0, token1, tradersToken0[0], 1),
+    new Order(utils.parseEther('0.5'),
+      utils.parseEther('0.45'), token0, token1, tradersToken0[1], 2)],
+    sellOrdersToken1: [new Order(utils.parseEther('0.9'),
+      utils.parseEther('0.90111'), token1, token0, tradersToken1[0], 1),
+    new Order(utils.parseEther('0.45'),
+      utils.parseEther('0.45'), token1, token0, tradersToken1[1], 1)]
+  };
+};
+
+export const oneOrderSellingToken0IsObmittedTestInput = function (token0: Contract, token1: Contract,
+  tradersToken0: Wallet[], tradersToken1: Wallet[]):
+  TestCaseInput {
+  return {
+    fundingAMMToken0: utils.parseEther('10'),
+    fundingAMMToken1: utils.parseEther('10'),
+    sellOrdersToken0: [new Order(utils.parseEther('10'),
+      utils.parseEther('9.7'), token0, token1, tradersToken0[0], 1),
+    new Order(utils.parseEther('5'), // <--- this order would can not be traded fully against uniswap
+      utils.parseEther('4.9'), token0, token1, tradersToken0[0], 2)],
+    sellOrdersToken1: [new Order(utils.parseEther('10'),
+      utils.parseEther('9'), token1, token0, tradersToken1[1], 1)]
+  };
+};
+
+export const noSolutionTestInput = function (token0: Contract, token1: Contract,
+  tradersToken0: Wallet[], tradersToken1: Wallet[]):
+  TestCaseInput {
+  return {
+    fundingAMMToken0: utils.parseEther('10'),
+    fundingAMMToken1: utils.parseEther('10'),
+    sellOrdersToken0: [new Order(utils.parseEther('10'),
+      utils.parseEther('9.7'), token0, token1, tradersToken0[0], 1),
+    new Order(utils.parseEther('5'), // <--- this order would can not be traded fully against uniswap
+      utils.parseEther('4.9'), token0, token1, tradersToken0[0], 2)],
+    sellOrdersToken1: [new Order(utils.parseEther('3'),
+      utils.parseEther('2'), token1, token0, tradersToken1[1], 1),
+    new Order(utils.parseEther('3'),
+      utils.parseEther('2.5'), token1, token0, tradersToken1[1], 2),
+    new Order(utils.parseEther('3'),
+      utils.parseEther('2.6'), token1, token0, tradersToken1[1], 3)]
+  };
+};
+
+export const oneOrderSellingToken1IsObmittedTestInput = function (token0: Contract, token1: Contract,
+  tradersToken0: Wallet[], tradersToken1: Wallet[]):
+  TestCaseInput {
+  return {
+    fundingAMMToken0: utils.parseEther('10'),
+    fundingAMMToken1: utils.parseEther('10'),
+
+    sellOrdersToken0: [new Order(utils.parseEther('10'),
+      utils.parseEther('9'), token1, token0, tradersToken1[1], 1),
+    new Order(utils.parseEther('3'),
+      utils.parseEther('2.5'), token1, token0, tradersToken1[1], 2),
+    new Order(utils.parseEther('3'),
+      utils.parseEther('2.5'), token1, token0, tradersToken1[1], 3),
+    new Order(utils.parseEther('3'), // <--- this order would can not be traded fully against uniswap
+      utils.parseEther('2.99'), token1, token0, tradersToken1[1], 3)],
+    sellOrdersToken1: [new Order(utils.parseEther('10'),
+      utils.parseEther('9.7'), token0, token1, tradersToken0[0], 1),
+    new Order(utils.parseEther('5'),
+      utils.parseEther('4.9'), token0, token1, tradersToken0[0], 2)]
+  };
+};
+
+export const switchTokenTestInput = function (token0: Contract, token1: Contract,
+  tradersToken0: Wallet[], tradersToken1: Wallet[]):
+  TestCaseInput {
+  return {
+    fundingAMMToken0: utils.parseEther('10'),
+    fundingAMMToken1: utils.parseEther('10'),
+    sellOrdersToken0: [new Order(utils.parseEther('10'),
+      utils.parseEther('9.7'), token0, token1, tradersToken0[0], 1),
+    new Order(utils.parseEther('5'),
+      utils.parseEther('4.9'), token0, token1, tradersToken0[0], 2)],
+    sellOrdersToken1: [new Order(utils.parseEther('10'),
+      utils.parseEther('9'), token1, token0, tradersToken1[1], 1),
+    new Order(utils.parseEther('3'),
+      utils.parseEther('2.5'), token1, token0, tradersToken1[1], 2),
+    new Order(utils.parseEther('3'),
+      utils.parseEther('2.5'), token1, token0, tradersToken1[1], 3),
+    new Order(utils.parseEther('3'),
+      utils.parseEther('2.99'), token1, token0, tradersToken1[1], 3)]
+  };
+};
