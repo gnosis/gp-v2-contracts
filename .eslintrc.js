4d313eddd09b40d1f0ceab82f1d750b9f2f80986
Ben Smith <bh2smith@gmail.com>
2020-09-21 16:12:44 +0200

    add new rc files

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    es2020: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  ignorePatterns: [
+    "abis/",
+    "build/",
+    "generated/",
+    "node_modules/",
+    "!.prettierrc.js",
+  ],
+}
