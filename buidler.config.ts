851c9f5606c9e617f2aac8a474ce98d9cf1dc8b6
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-09-24 11:47:33 +0200

    remove sample accounts task

--- buidler.config.ts
+++ buidler.config.ts
@@ -2,14 +2,6 @@ import { task, usePlugin } from "@nomiclabs/buidler/config";
 
 usePlugin("@nomiclabs/buidler-waffle");
 
-task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.getAddress());
-  }
-});
-
 export default {
   paths: {
     sources: "src/contracts",
