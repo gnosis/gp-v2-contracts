20294d90023e4798a7b4cb5f53c2596da33dc99a
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-10-22 20:01:16 +0200

    use Uniswap pair address to uniquely identify token pairs

--- test/GPv2Settlement.test.ts
+++ test/GPv2Settlement.test.ts
@@ -1,5 +1,7 @@
 import { ethers, waffle } from "@nomiclabs/buidler";
+import IERC20 from "@openzeppelin/contracts/build/contracts/IERC20.json";
 import IUniswapV2Factory from "@uniswap/v2-core/build/IUniswapV2Factory.json";
+import UniswapV2Factory from "@uniswap/v2-core/build/UniswapV2Factory.json";
 import { expect } from "chai";
 import { Contract } from "ethers";
 
@@ -43,23 +45,69 @@ describe("GPv2Settlement", () => {
     });
   });
 
-  describe("pairId", () => {
-    it("should return the same ID regardless of token order", async () => {
+  describe("uniswapPairAddress", () => {
+    it("should match the on-chain Uniswap pair address", async () => {
+      // TODO(nlordell): This should move to be an integration test once they
+      // have been setup.
+
+      const tokenA = await waffle.deployMockContract(deployer, IERC20.abi);
+      const tokenB = await waffle.deployMockContract(deployer, IERC20.abi);
+      const [token0, token1] =
+        tokenA.address < tokenB.address ? [tokenA, tokenB] : [tokenB, tokenA];
+
+      const GPv2Settlement = await ethers.getContractFactory("GPv2Settlement");
+      for (const [tokenX, tokenY] of [
+        [token0, token1],
+        [token1, token0],
+      ]) {
+        const uniswapFactory = await waffle.deployContract(
+          deployer,
+          UniswapV2Factory,
+          [deployer.address],
+        );
+
+        await uniswapFactory.createPair(tokenX.address, tokenY.address);
+        const uniswapPairAddress = await uniswapFactory.getPair(
+          tokenX.address,
+          tokenY.address,
+        );
+
+        const settlement = await GPv2Settlement.deploy(uniswapFactory.address);
+
+        expect(
+          await settlement.uniswapPairAddress(token0.address, token1.address),
+        ).to.equal(uniswapPairAddress);
+      }
+    });
+
+    it("should revert if token order is inverted", async () => {
       const [token0, token1] = [
-        `0x${"42".repeat(20)}`,
-        `0x${"1337".repeat(10)}`,
+        `0x${"00".repeat(19)}0a`,
+        `0x${"00".repeat(19)}0b`,
       ];
 
-      expect(await settlement.pairId(token0, token1)).to.equal(
-        await settlement.pairId(token1, token0),
-      );
+      await expect(
+        settlement.uniswapPairAddress(token1, token0),
+      ).to.be.revertedWith("invalid pair");
     });
 
     it("should revert for pairs where both tokens are equal", async () => {
       const token = `0x${"42".repeat(20)}`;
-      await expect(settlement.pairId(token, token)).to.be.revertedWith(
-        "invalid pair",
-      );
+
+      await expect(
+        settlement.uniswapPairAddress(token, token),
+      ).to.be.revertedWith("invalid pair");
+    });
+
+    it("should revert for pairs where either token is address 0", async () => {
+      const token = `0x${"42".repeat(20)}`;
+
+      await expect(
+        settlement.uniswapPairAddress(token, ethers.constants.AddressZero),
+      ).to.be.revertedWith("invalid pair");
+      await expect(
+        settlement.uniswapPairAddress(ethers.constants.AddressZero, token),
+      ).to.be.revertedWith("invalid pair");
     });
   });
 });
