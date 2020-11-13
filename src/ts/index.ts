ca42a078eb83a8be98b1d932e832d18f376f4d79
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-11-13 16:58:58 +0100

    improve documentation

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -27,7 +27,11 @@ export interface EIP712Domain {
 }
 
 /**
- * Return the Gnosis Protocol v2 domain used for EIP-712 signing.
+ * Return the Gnosis Protocol v2 domain used for signing.
+ * @param chainId The EIP-155 chain ID.
+ * @param verifyingContract The address of the contract that will verify the
+ * signature.
+ * @return An EIP-712 compatible typed domain data.
  */
 export function domain(
   chainId: number,
