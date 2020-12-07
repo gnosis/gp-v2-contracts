db72fa8bf2b336bfb1200205230d1186dc6083c1
Benjamin Smith <bh2smith@users.noreply.github.com>
2020-12-07 12:09:21 +0100

    Update src/contracts/interfaces/GPv2Authentication.sol
    Co-authored-by: Nicholas Rodrigues Lordello <nlordell@gmail.com>

--- src/contracts/interfaces/GPv2Authentication.sol
+++ src/contracts/interfaces/GPv2Authentication.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-newer
-pragma solidity >=0.6.12 <0.8.0;
+pragma solidity ^0.7.5;
 
 /// @title Gnosis Protocol v2 Authentication Interface
 /// @author Gnosis Developers
