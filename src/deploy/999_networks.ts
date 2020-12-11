814d2400528cdadb9a8f9e49a5a5d7e86f7d453d
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-12-11 12:51:31 +0100

    Add networks.json

---
+++ src/deploy/999_networks.ts
@@ -0,0 +1,63 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+import { Deployment, DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const NETWORKS_PATH = path.join(__dirname, "../../networks.json");
+const INDENT = "  ";
+
+type Networks = Record<string, Network>;
+type Network = Record<number, DeploymentRecord>;
+
+interface DeploymentRecord {
+  address: string;
+  transactionHash?: string;
+  deploymentArguments?: unknown[];
+}
+
+const updateNetworks: DeployFunction = async function ({
+  deployments,
+  getChainId,
+  ethers,
+  network,
+}: HardhatRuntimeEnvironment) {
+  if (network.name === "hardhat") {
+    return;
+  }
+
+  console.log("updating 'networks.json'...");
+
+  const chainId = parseInt(await getChainId());
+  const networks: Networks = JSON.parse(
+    await fs.readFile(NETWORKS_PATH, "utf-8"),
+  );
+
+  const initializeRecord = (contractName: string, address: string) => {
+    networks[contractName] = networks[contractName] || {};
+    return (networks[contractName][chainId] = {
+      ...networks[contractName][chainId],
+      address,
+    });
+  };
+
+  for (const [name, deployment] of Object.entries(await deployments.all())) {
+    const { address, transactionHash, args } = deployment;
+    const record = initializeRecord(name, address);
+
+    // NOTE: Preserve transaction hash and arguments in case there is no new
+    // deployment because the contract bytecode did not change.
+    record.transactionHash = transactionHash || record.transactionHash;
+    record.deploymentArguments = args || record.deploymentArguments;
+  }
+
+  const settlement = await ethers.getContractAt(
+    "GPv2Settlement",
+    networks["GPv2Settlement"][chainId].address,
+  );
+  initializeRecord("GPv2AllowanceManager", await settlement.allowanceManager());
+
+  await fs.writeFile(NETWORKS_PATH, JSON.stringify(networks, null, INDENT));
+};
+
+export default updateNetworks;
