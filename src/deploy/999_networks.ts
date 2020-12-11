0962fd705c684be4213e56511c27557434b38a97
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-12-11 17:35:48 +0100

    include allowance manager transaction hash

--- src/deploy/999_networks.ts
+++ src/deploy/999_networks.ts
@@ -32,29 +32,34 @@ const updateNetworks: DeployFunction = async function ({
     await fs.readFile(NETWORKS_PATH, "utf-8"),
   );
 
-  const initializeRecord = (contractName: string, address: string) => {
+  const updateRecord = (
+    contractName: string,
+    { address, transactionHash }: DeploymentRecord,
+  ) => {
     networks[contractName] = networks[contractName] || {};
-    return (networks[contractName][chainId] = {
+    const record = (networks[contractName][chainId] = {
       ...networks[contractName][chainId],
       address,
     });
-  };
-
-  for (const [name, { address, transactionHash }] of Object.entries(
-    await deployments.all(),
-  )) {
-    const record = initializeRecord(name, address);
 
     // NOTE: Preserve transaction hash in case there is no new deployment
     // because the contract bytecode did not change.
     record.transactionHash = transactionHash || record.transactionHash;
+  };
+
+  for (const [name, deployment] of Object.entries(await deployments.all())) {
+    updateRecord(name, deployment);
   }
 
+  const settlementRecord = networks["GPv2Settlement"][chainId];
   const settlement = await ethers.getContractAt(
     "GPv2Settlement",
-    networks["GPv2Settlement"][chainId].address,
+    settlementRecord.address,
   );
-  initializeRecord("GPv2AllowanceManager", await settlement.allowanceManager());
+  updateRecord("GPv2AllowanceManager", {
+    address: await settlement.allowanceManager(),
+    transactionHash: settlementRecord.transactionHash,
+  });
 
   await fs.writeFile(NETWORKS_PATH, JSON.stringify(networks, null, INDENT));
 };
