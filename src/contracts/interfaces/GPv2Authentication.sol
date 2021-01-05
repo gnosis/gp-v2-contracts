d785bf04e49eb5090c668377405da944f8c6a06e
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-01-05 08:56:28 +0100

    update SDPX to use deprecated LGPL 3 or newer identifier

--- src/contracts/interfaces/GPv2Authentication.sol
+++ src/contracts/interfaces/GPv2Authentication.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-3.0
+// SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.7.6;
 
 /// @title Gnosis Protocol v2 Authentication Interface
--- src/contracts/ownership/CustomInitiallyOwnable.sol
+++ src/contracts/ownership/CustomInitiallyOwnable.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-3.0
+// SPDX-License-Identifier: LGPL-3.0-or-later
 pragma solidity ^0.7.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
