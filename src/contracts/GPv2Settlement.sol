bf35be7b9b7acd73e1471c0d859108e3941d5f23
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-01-06 18:29:20 +0100

    add param desciption

--- src/contracts/GPv2Settlement.sol
+++ src/contracts/GPv2Settlement.sol
@@ -374,6 +374,8 @@ contract GPv2Settlement {
 
     /// @dev Claims order gas refunds by freeing storage for all encoded order
     /// gas refunds.
+    ///
+    /// @param orderUids The order unique identifiers as packed bytes.
     function claimOrderRefunds(bytes calldata encodedOrderRefunds) internal {
         for (
             uint256 i = 0;
