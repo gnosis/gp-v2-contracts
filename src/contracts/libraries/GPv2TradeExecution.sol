e898fd49bf810969b3d78d0e02265de673103363
josojo <josojo@hotmail.de>
2021-01-08 23:31:04 +0100

    remove unwanted code

--- src/contracts/libraries/GPv2TradeExecution.sol
+++ src/contracts/libraries/GPv2TradeExecution.sol
@@ -41,32 +41,7 @@ library GPv2TradeExecution {
         if (address(trade.buyToken) == BUY_ETH_ADDRESS) {
             payable(trade.owner).transfer(trade.buyAmount);
         } else {
-            string memory errMsg =
-                toString(
-                    abi.encodePacked(
-                        "buffer of token ",
-                        trade.buyToken,
-                        "not sufficient"
-                    )
-                );
-            require(
-                trade.buyToken.balanceOf(address(this)) > trade.buyAmount,
-                errMsg
-            );
             trade.buyToken.safeTransfer(trade.owner, trade.buyAmount);
         }
-    }
-
-    function toString(bytes memory data) public pure returns (string memory) {
-        bytes memory alphabet = "0123456789abcdef";
-
-        bytes memory str = new bytes(2 + data.length * 2);
-        str[0] = "0";
-        str[1] = "x";
-        for (uint256 i = 0; i < data.length; i++) {
-            str[2 + i * 2] = alphabet[uint256(uint8(data[i] >> 4))];
-            str[3 + i * 2] = alphabet[uint256(uint8(data[i] & 0x0f))];
-        }
-        return string(str);
     }
 }
