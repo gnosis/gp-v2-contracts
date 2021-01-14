483e26ffcd84d213e1647397e073041129c07dec
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-01-14 16:29:24 +0100

    tracing with local functions

--- bench/trace/gas.ts
+++ bench/trace/gas.ts
@@ -1,4 +1,5 @@
 import { BigNumber, BigNumberish, ContractReceipt } from "ethers";
+import Debug from "debug";
 import { ethers } from "hardhat";
 import {
   CallMessageTrace,
@@ -6,20 +7,26 @@ import {
   isCallTrace,
   isEvmStep,
   isPrecompileTrace,
+  MessageTraceStep,
 } from "hardhat/internal/hardhat-network/stack-traces/message-trace";
+import { JumpType } from "hardhat/internal/hardhat-network/stack-traces/model";
+
+const debug = Debug("bench:trace:gas");
 
 export interface GasTrace {
   name: string;
   gas: BigNumber;
+  cumulativeGas: BigNumber;
   children: GasTrace[];
 }
 
 function node(
   name: string,
   gas: BigNumberish,
+  cumulativeGas: BigNumberish,
   children?: GasTrace[],
 ): GasTrace {
-  return { name, gas: BigNumber.from(gas), children: children || [] };
+  return { name, gas: BigNumber.from(gas), cumulativeGas: BigNumber.from(cumulativeGas), children: children || [] };
 }
 
 export function decodeGasTrace(
@@ -37,10 +44,10 @@ export function decodeGasTrace(
     0,
   );
 
-  return node(callName(trace), tx.gasUsed, [
-    node("<base>", baseGas),
-    node("<calldata>", calldataGas),
-    entrypoint(trace),
+  return node(callName(trace), tx.gasUsed, 0, [
+    node("<base>", baseGas, 0),
+    node("<calldata>", calldataGas, baseGas),
+    entrypoint(trace, baseGas + calldataGas),
   ]);
 }
 
@@ -64,27 +71,86 @@ function callName({ address, bytecode, calldata }: CallMessageTrace): string {
   }`;
 }
 
-function entrypoint({ steps, gasUsed }: CallMessageTrace): GasTrace {
-  const children = [];
-  for (const step of steps) {
-    if (isEvmStep(step)) {
-      // TODO: Try and keep an internal function stack trace, although this
-      // has proven quite challenging with optimizations turned on.
-    } else if (isCallTrace(step)) {
-      children.push(externFunction(step));
-    } else if (isPrecompileTrace(step)) {
-      children.push(
-        node(`<precompile 0x${step.precompile}>`, step.gasUsed.toString()),
-      );
-    }
-  }
-
-  return node("<entrypoint>", gasUsed.toString(), children);
+function entrypoint(trace: CallMessageTrace, startingGas: BigNumberish): GasTrace {
+  const tracer = new CodeTracer(trace, startingGas);
+  const name = "<entrypoint>";
+  return node(name, trace.gasUsed.toString(), 0, tracer.traceToEnd(name));
 }
 
-function externFunction(
-  trace: CallMessageTrace | DecodedCallMessageTrace,
-): GasTrace {
-  const { children } = entrypoint(trace);
-  return node(callName(trace), trace.gasUsed.toString(), children);
+type GasExtended<T> = T & {
+  gasLeft?: unknown;
+  gasRefund?: unknown;
+}
+
+class CodeTracer {
+  private stepIndex = -1;
+
+  constructor(
+    public readonly trace: CallMessageTrace,
+    private readonly startingGas: BigNumberish,
+  ) {}
+
+  get step(): GasExtended<MessageTraceStep> {
+    return this.trace.steps[this.stepIndex];
+  }
+
+  private get cumulativeGas(): BigNumber {
+    return BigNumber.from(this.startingGas);
+  }
+
+  private nextStep(): GasExtended<MessageTraceStep> {
+    this.stepIndex++;
+    return this.step;
+  }
+
+  public traceToEnd(name: string): GasTrace[] {
+    const nodes = this.traceLocal(name);
+    if (this.step) {
+      throw new Error("trace to end with remaining steps")
+    }
+
+    return nodes;
+  }
+
+  private traceLocal(name: string): GasTrace[] {
+    debug(`>>> entering ${name}`);
+    
+    const nodes = [];
+    let step;
+    while (step = this.nextStep()) {
+      const { gasLeft, gasRefund } = step;
+      if (isEvmStep(step)) {
+        // TODO: Stack trace is not very accurate with optimizations turned on.
+        const instruction = this.trace.bytecode?.getInstruction(step.pc)
+        const jumpType = instruction?.jumpType;
+        if (jumpType == JumpType.INTO_FUNCTION) {
+            const name = instruction?.location?.getContainingFunction()?.name || "<unknown>";
+            const startingGas = BigNumber.from(`${gasLeft || 0}`);
+            const children = this.traceLocal(name);
+            const endingGas = BigNumber.from(`${this.step?.gasLeft || 0}`);
+            nodes.push(node(
+              name,
+              startingGas.sub(endingGas),
+              this.cumulativeGas,
+              children,
+            ))
+        } else if (jumpType == JumpType.OUTOF_FUNCTION) {
+          break;
+        }
+      } else if (isCallTrace(step)) {
+        const subTracer = new CodeTracer(step, this.startingGas);
+        const name = callName(step);
+        nodes.push(
+          node(name, step.gasUsed.toString(), this.cumulativeGas, subTracer.traceToEnd(name)),
+        );
+      } else if (isPrecompileTrace(step)) {
+        nodes.push(
+          node(`<precompile 0x${step.precompile}>`, step.gasUsed.toString(), this.cumulativeGas),
+        );
+      }
+    }
+
+    debug(`<<< exiting ${name}`);
+    return nodes;
+  }
 }
