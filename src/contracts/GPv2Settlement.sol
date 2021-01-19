3dfc0a85860a1ab95ebce794b69334683d16ec54
Ben Smith <bh2smith@gmail.com>
2021-01-19 10:03:57 +0100

    rename fee discount error message

--- src/contracts/GPv2Settlement.sol
+++ src/contracts/GPv2Settlement.sol
@@ -313,7 +313,7 @@ contract GPv2Settlement {
             );
         }
 
-        require(trade.feeDiscount <= BPS_BASE, "GPv2: invalid fee discount");
+        require(trade.feeDiscount <= BPS_BASE, "GPv2: fee discount too large");
         executedFeeAmount =
             executedFeeAmount.mul(BPS_BASE - trade.feeDiscount) /
             BPS_BASE;
