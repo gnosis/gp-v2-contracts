5c3f0b7efb0420b5ce2586b638af7e406dd6bfd3
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2021-01-25 12:10:14 +0100

    Suppress Solc compilation warning in test contract

--- src/contracts/test/StateChangingERC1271.sol
+++ src/contracts/test/StateChangingERC1271.sol
@@ -16,5 +16,13 @@ contract StateChangingEIP1271 {
     {
         state += 1;
         magicValue = GPv2EIP1271.MAGICVALUE;
+
+        // The following lines are there to suppress no-unused-var compiler-time
+        // warnings when compiling the contracts. The warning is forwarded by
+        // Hardhat from Solc. It is currently not possible to selectively
+        // ignore Solc warinings:
+        // <https://github.com/ethereum/solidity/issues/269>
+        _hash;
+        _signature;
     }
 }
