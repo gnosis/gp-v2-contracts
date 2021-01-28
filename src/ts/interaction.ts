2b7b391f766e957ce51624cb29d2d15cd6c53759
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-01-28 14:50:01 +0100

    add documentation

--- src/ts/interaction.ts
+++ src/ts/interaction.ts
@@ -21,6 +21,12 @@ export interface Interaction {
 export type InteractionLike = Pick<Interaction, "target"> &
   Partial<Interaction>;
 
+/**
+ * Normalizes interaction data so that it is ready to be be ABI encoded.
+ *
+ * @param interaction The interaction to normalize.
+ * @return The normalized interaction.
+ */
 export function normalizeInteraction(
   interaction: InteractionLike,
 ): Interaction {
@@ -31,6 +37,13 @@ export function normalizeInteraction(
   };
 }
 
+/**
+ * Normalizes data for many interactions so that they can be ABI encoded. This
+ * calls [`normalizeInteraction`] for each interaction.
+ *
+ * @param interactions The interactions to normalize.
+ * @return The normalized interactions.
+ */
 export function normalizeInteractions(
   interactions: InteractionLike[],
 ): Interaction[] {
