9414ed82480010d5bf9178a38b27f1689c750620
Ben Smith <bh2smith@gmail.com>
2021-02-03 15:26:02 +0100

    revert onlyProxyAdmin for another PR

--- src/contracts/GPv2AllowListAuthentication.sol
+++ src/contracts/GPv2AllowListAuthentication.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.7.6;
 
 import "@gnosis.pm/util-contracts/contracts/StorageAccessible.sol";
 import "@openzeppelin/contracts/utils/EnumerableSet.sol";
-import "@openzeppelin/contracts/proxy/Proxy.sol";
 import "./interfaces/GPv2Authentication.sol";
 
 /// @title Gnosis Protocol v2 Access Control Contract
@@ -20,27 +19,9 @@ contract GPv2AllowListAuthentication is GPv2Authentication, StorageAccessible {
         manager = manager_;
     }
 
-    modifier onlyProxyAdmin() {
-        // Slot taken from https://eips.ethereum.org/EIPS/eip-1967#specification
-        // obtained as bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1)
-        bytes32 slot =
-            0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103;
-        address proxyAdmin;
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            proxyAdmin := sload(slot)
-        }
-        require(msg.sender == proxyAdmin, "GPv2: caller not proxyAdmin");
-        _;
-    }
-
     modifier onlyManager() {
         require(manager == msg.sender, "GPv2: caller not manager");
         _;
-    }
-
-    function updateManager(address newManager) external onlyProxyAdmin {
-        manager = newManager;
     }
 
     function addSolver(address solver) external onlyManager {
