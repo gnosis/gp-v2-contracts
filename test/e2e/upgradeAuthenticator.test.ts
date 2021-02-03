4424549857e10d33af8b567c6fb976fe3d38f6b9
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-02-03 20:34:33 +0100

    more tests

--- test/e2e/upgradeAuthenticator.test.ts
+++ test/e2e/upgradeAuthenticator.test.ts
@@ -4,10 +4,37 @@ import { deployments, ethers } from "hardhat";
 
 import { deployTestContracts } from "./fixture";
 
+async function rejectError(
+  promise: Promise<unknown>,
+): Promise<Error | undefined> {
+  try {
+    await promise;
+    return undefined;
+  } catch (err) {
+    return err;
+  }
+}
+
+async function upgrade(
+  proxyOwner: Wallet,
+  contractName: string,
+  newContractName: string,
+) {
+  // Note that deterministic deployment and gasLimit are not needed/used here as deployment args.
+  await deployments.deploy(contractName, {
+    contract: newContractName,
+    // From differs from initial deployment here since the proxy owner is the Authenticator manager.
+    from: proxyOwner.address,
+    proxy: true,
+  });
+}
+
 describe("Upgrade Authenticator", () => {
   let authenticator: Contract;
   let deployer: Wallet;
   let owner: Wallet;
+  let nonOwner: Wallet;
+  let newManager: Wallet;
   let solver: Wallet;
 
   beforeEach(async () => {
@@ -15,7 +42,7 @@ describe("Upgrade Authenticator", () => {
       authenticator,
       deployer,
       owner,
-      wallets: [solver],
+      wallets: [nonOwner, newManager, solver],
     } = await deployTestContracts());
   });
 
@@ -32,6 +59,7 @@ describe("Upgrade Authenticator", () => {
     await expect(authenticatorV2.newMethod()).to.be.reverted;
 
     await upgrade(
+      owner,
       "GPv2AllowListAuthentication",
       "GPv2AllowListAuthenticationV2",
     );
@@ -40,10 +68,12 @@ describe("Upgrade Authenticator", () => {
   });
 
   it("should preserve storage", async () => {
-    await authenticator.connect(owner).addSolver(solver.address);
+    await authenticator.connect(owner).setManager(newManager.address);
+    await authenticator.connect(newManager).addSolver(solver.address);
 
-    // Upgrade after storage is set.
+    // Upgrade after storage is set with **proxy owner**;
     await upgrade(
+      owner,
       "GPv2AllowListAuthentication",
       "GPv2AllowListAuthenticationV2",
     );
@@ -55,18 +85,34 @@ describe("Upgrade Authenticator", () => {
     const authenticatorV2 = GPv2AllowListAuthenticationV2.attach(
       authenticator.address,
     );
-    // Both, the listed solvers and original manager are still set
+
+    // Both, the listed solvers and updated manager are still set
     expect(await authenticatorV2.isSolver(solver.address)).to.equal(true);
-    expect(await authenticatorV2.manager()).to.equal(owner.address);
+    expect(await authenticatorV2.manager()).to.equal(newManager.address);
   });
 
-  async function upgrade(contractName: string, newContractName: string) {
-    // Note that deterministic deployment and gasLimit are not needed/used here as deployment args.
-    await deployments.deploy(contractName, {
-      contract: newContractName,
-      // From differs from initial deployment here since the proxy owner is the Authenticator manager.
-      from: owner.address,
-      proxy: true,
-    });
-  }
+  it("should revert when not upgrading with the authentication manager", async () => {
+    await authenticator.connect(owner).setManager(newManager.address);
+    expect(
+      await rejectError(
+        upgrade(
+          newManager,
+          "GPv2AllowListAuthentication",
+          "GPv2AllowListAuthenticationV2",
+        ),
+      ),
+    ).to.not.be.undefined;
+  });
+
+  it("should revert when not upgrading with the proxy owner", async () => {
+    expect(
+      await rejectError(
+        upgrade(
+          nonOwner,
+          "GPv2AllowListAuthentication",
+          "GPv2AllowListAuthenticationV2",
+        ),
+      ),
+    ).to.not.be.undefined;
+  });
 });
