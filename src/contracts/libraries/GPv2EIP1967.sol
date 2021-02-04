12ba1d47186faf1b4a76af57e17bccc80e8c5633
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-02-04 20:51:52 +0100

    remove dead code

--- src/contracts/libraries/GPv2EIP1967.sol
+++ src/contracts/libraries/GPv2EIP1967.sol
@@ -2,48 +2,15 @@
 pragma solidity ^0.7.6;
 
 library GPv2EIP1967 {
-    /// @dev The storage slot where the proxy implementation is stored, defined
-    /// as `keccak256('eip1967.proxy.implementation') - 1`.
-    bytes32 internal constant IMPLEMENTATION_SLOT =
-        hex"360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
-
     /// @dev The storage slot where the proxy administrator is stored, defined
     /// as `keccak256('eip1967.proxy.admin') - 1`.
     bytes32 internal constant ADMIN_SLOT =
         hex"b53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103";
 
-    /// @dev Returns the address stored in the EIP-1967 implementation storage
-    /// slot for the current contract. If this method is not called from an
-    /// EIP-1967 contract, then it will likely return `address(0)`, as the
-    /// implementation slot is likely to be unset.
-    ///
-    /// @return implementation The implementation address.
-    function getImplementation()
-        internal
-        view
-        returns (address implementation)
-    {
-        // NOTE: Assembly is required for reading and writing storage at
-        // arbirary slots.
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            implementation := sload(IMPLEMENTATION_SLOT)
-        }
-    }
-
-    /// @dev Sets the storage at the EIP-1967 implementation slot to be the
-    /// specified address.
-    ///
-    /// @param implementation The implementation address to set.
-    function setImplementation(address implementation) internal {
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            sstore(IMPLEMENTATION_SLOT, implementation)
-        }
-    }
-
     /// @dev Returns the address stored in the EIP-1967 administrator storage
-    /// slot for the current contract.
+    /// slot for the current contract. If this method is not called from an
+    /// contract behind an EIP-1967 proxy, then it will most likely return
+    /// `address(0)`, as the implementation slot is likely to be unset.
     ///
     /// @return admin The administrator address.
     function getAdmin() internal view returns (address admin) {
