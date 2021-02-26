2ae20949df00b3bec4987653f5b1f7e3c8773462
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2021-02-26 10:14:26 +0100

    Remove single trade settlement (#467)
    Closes #466
    
    This PR removes single trade settlements from the settlement contract because they can be exploited by malicious solvers to extract user funds (see #466).
    
    ### Test Plan
    
    Removed code and related unit tests. CI should still pass and we **should not** see a drop in coverage.

--- bench/single.ts
+++ bench/single.ts
@@ -7,44 +7,27 @@ async function main() {
 
   const pad = (x: unknown) => ` ${x} `.padStart(14);
   console.log(chalk.bold("=== Single Order Gas Benchmarks ==="));
+  console.log(chalk.gray("--------------+--------------"));
   console.log(
-    chalk.gray("--------------+--------------+--------------+--------------"),
-  );
-  console.log(
-    ["settlement", "include fees", "gasToken", "gas"]
+    ["gasToken", "gas"]
       .map((header) => chalk.cyan(pad(header)))
       .join(chalk.gray("|")),
   );
-  console.log(
-    chalk.gray("--------------+--------------+--------------+--------------"),
-  );
-  for (const [kind, includeFees] of [
-    ["standard", undefined],
-    ["single", true],
-    ["single", false],
-  ] as const) {
-    for (const gasToken of [0, 5]) {
-      const { gasUsed } =
-        kind === "standard"
-          ? await fixture.settle({
-              tokens: 2,
-              trades: 1,
-              interactions: 1,
-              refunds: 0,
-              gasToken,
-            })
-          : await fixture.settleOrder({
-              includeFees: includeFees === true,
-              gasToken,
-            });
+  console.log(chalk.gray("--------------+--------------"));
+  for (const gasToken of [0, 5]) {
+    const { gasUsed } = await fixture.settle({
+      tokens: 2,
+      trades: 1,
+      interactions: 1,
+      refunds: 0,
+      gasToken,
+    });
 
-      console.log(
-        [
-          ...[kind, includeFees ?? "-", gasToken].map(pad),
-          chalk.yellow(pad(gasUsed.toString())),
-        ].join(chalk.gray("|")),
-      );
-    }
+    console.log(
+      [pad(gasToken), chalk.yellow(pad(gasUsed.toString()))].join(
+        chalk.gray("|"),
+      ),
+    );
   }
 }
 
--- src/ts/settlement.ts
+++ src/ts/settlement.ts
@@ -142,36 +142,6 @@ export type EncodedSettlement = [
 ];
 
 /**
- * Direct transfers used in "fast-path" for settling single trades.
- */
-export interface Transfer {
-  /** Receiver of the user funds. */
-  target: string;
-  /** Amount to transfer. */
-  amount: BigNumberish;
-}
-
-/**
- * Encoded single trade settlement parameters.
- */
-export type EncodedSingleTradeSettlement = [
-  /** Tokens. */
-  [string, string],
-  /** Encoded trade. */
-  Trade,
-  /** Encoded transfers. */
-  Transfer[],
-  /** Encoded interactions for executing a single trade order. */
-  Interaction[],
-];
-
-/**
- * Maximum number of trades that can be included in a single call to the settle
- * function.
- */
-export const MAX_TRADES_IN_SETTLEMENT = 2 ** 16 - 1;
-
-/**
  * Encodes signing scheme as a bitfield.
  *
  * @param scheme The signing scheme to encode.
@@ -312,22 +282,6 @@ export class SettlementEncoder {
   }
 
   /**
-   * Returns whether the currently encoded settlement can be executed with the
-   * `settleOne` single trade settlement "fast-path".
-   */
-  public get isSingleTradeSettlement(): boolean {
-    const [preInteractions, , postInteractions] = this.interactions;
-    const { filledAmounts, preSignatures } = this.orderRefunds;
-    return (
-      this.tokens.length === 2 &&
-      this.trades.length === 1 &&
-      [preInteractions, postInteractions, filledAmounts, preSignatures].every(
-        ({ length }) => length === 0,
-      )
-    );
-  }
-
-  /**
    * Returns a clearing price vector for the current settlement tokens from the
    * provided price map.
    *
@@ -455,30 +409,6 @@ export class SettlementEncoder {
       this.trades,
       this.interactions,
       this.orderRefunds,
-    ];
-  }
-
-  /**
-   * Returns the encoded single trade settlement parameters.
-   */
-  public encodeSingleTradeSettlement(
-    transfers: Transfer[],
-  ): EncodedSingleTradeSettlement {
-    if (!this.isSingleTradeSettlement) {
-      throw new Error("cannot be encoded as a single trade settlement");
-    }
-
-    const [token0, token1] = this.tokens;
-    const trade = {
-      ...this.trades[0],
-      executedAmount: ethers.constants.Zero,
-    };
-
-    return [
-      [token0, token1],
-      trade,
-      transfers,
-      this.interactions[InteractionStage.INTRA],
     ];
   }
 
