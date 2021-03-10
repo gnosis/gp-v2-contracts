05d31e672ec5689bdc55cf7cdac591f722189613
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-03-10 19:27:02 +0100

    Remove hardhat imports from exported library code

--- src/ts/proxy.ts
+++ src/ts/proxy.ts
@@ -1,8 +1,6 @@
 // defined in https://eips.ethereum.org/EIPS/eip-1967
 
-import { BigNumber, Contract } from "ethers";
-import { ethers } from "hardhat";
-import Proxy from "hardhat-deploy/extendedArtifacts/EIP173Proxy.json";
+import { BigNumber, Contract, ethers } from "ethers";
 
 // The proxy contract used by hardhat-deploy implements EIP-1967 (Standard Proxy
 // Storage Slot). See <https://eips.ethereum.org/EIPS/eip-1967>.
@@ -22,10 +20,13 @@ const OWNER_STORAGE_SLOT = slot("eip1967.proxy.admin");
  * @param proxy Address of the proxy contract.
  * @returns The address of the contract storing the proxy implementation.
  */
-export async function implementationAddress(proxy: string): Promise<string> {
+export async function implementationAddress(
+  provider: ethers.providers.Provider,
+  proxy: string,
+): Promise<string> {
   const [implementation] = ethers.utils.defaultAbiCoder.decode(
     ["address"],
-    await ethers.provider.getStorageAt(proxy, IMPLEMENTATION_STORAGE_SLOT),
+    await provider.getStorageAt(proxy, IMPLEMENTATION_STORAGE_SLOT),
   );
   return implementation;
 }
@@ -37,13 +38,28 @@ export async function implementationAddress(proxy: string): Promise<string> {
  * @param proxy Address of the proxy contract.
  * @returns The address of the administrator of the proxy.
  */
-export async function ownerAddress(proxy: string): Promise<string> {
+export async function ownerAddress(
+  provider: ethers.providers.Provider,
+  proxy: string,
+): Promise<string> {
   const [owner] = ethers.utils.defaultAbiCoder.decode(
     ["address"],
-    await ethers.provider.getStorageAt(proxy, OWNER_STORAGE_SLOT),
+    await provider.getStorageAt(proxy, OWNER_STORAGE_SLOT),
   );
   return owner;
 }
+
+/**
+ * EIP-173 proxy ABI in "Human-readable ABI format".
+ *
+ * <https://eips.ethereum.org/EIPS/eip-173#specification>
+ */
+export const EIP173_PROXY_ABI = [
+  "event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)",
+  "function owner() view external returns(address)",
+  "function transferOwnership(address newOwner) external",
+  "function supportsInterface(bytes4 interfaceID) external view returns (bool)",
+];
 
 /**
  * Returns the proxy interface for the specified address.
@@ -52,10 +68,9 @@ export async function ownerAddress(proxy: string): Promise<string> {
  * @returns A Ethers.js contract instance for interacting with the proxy.
  */
 export function proxyInterface(contract: Contract): Contract {
-  const { abi } = Proxy;
   return new Contract(
     contract.address,
-    abi,
+    EIP173_PROXY_ABI,
     contract.signer ?? contract.provider,
   );
 }
