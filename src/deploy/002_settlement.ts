8fe62364ff1726c6745824ed12997f41a02dffff
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2021-03-15 07:56:14 +0100

    Implement E2E test using Vault internal balances. (#514)
    This PR adds the E2E test that was promised in #513 that demonstrates a settlement with a trade that pays with internal Vault balances and another that receives proceeds in internal Vault balances.
    
    As the Vault source is currently private, this PR just adds vendored artifacts required for the Vault deployment. These were build with commit `4764739` (the full hash is included in a `.version` file). One issue, however, is that the Vault code size was larger than the limit. Instead of modifying our network configuration, I just manually deleted the flash loan functionality from the Vault in order to bring the code size below the limit.
    
    Additionally, a helper method was added to the `SettlementEncoder` for encoding "setup" settlements that just execute interactions for things like setting ERC20 allowances to other protocols.
    
    This is the test :tada:

--- src/deploy/002_settlement.ts
+++ src/deploy/002_settlement.ts
@@ -1,22 +1,46 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+import Authorizer from "../../test/e2e/balancer/Authorizer.json";
+import Vault from "../../test/e2e/balancer/Vault.json";
 import { CONTRACT_NAMES, SALT } from "../ts/deploy";
 
 const deploySettlement: DeployFunction = async function ({
   deployments,
+  ethers,
   getNamedAccounts,
+  network,
 }: HardhatRuntimeEnvironment) {
-  const { deployer, vault } = await getNamedAccounts();
+  const { deployer, manager } = await getNamedAccounts();
   const { deploy, get } = deployments;
 
   const { authenticator, settlement } = CONTRACT_NAMES;
   const { address: authenticatorAddress } = await get(authenticator);
 
+  let vaultAddress: string;
+  if (network.name === "hardhat") {
+    const { address: authorizerAddress } = await deploy("VaultAuthorizer", {
+      from: deployer,
+      contract: Authorizer,
+      gasLimit: 3e6,
+      args: [manager],
+    });
+    ({ address: vaultAddress } = await deploy("Vault", {
+      from: deployer,
+      contract: Vault,
+      gasLimit: 8e6,
+      args: [authorizerAddress],
+    }));
+  } else {
+    // TODO(nlordell): Once the Vault is deployed, we need to get the address
+    // based on the network.
+    vaultAddress = ethers.constants.AddressZero;
+  }
+
   await deploy(settlement, {
     from: deployer,
     gasLimit: 4e6,
-    args: [authenticatorAddress, vault],
+    args: [authenticatorAddress, vaultAddress],
     deterministicDeployment: SALT,
     log: true,
   });
