3f42ac241542e95419149250638ea036f74285f9
Leandro Boscariol <leandro.boscariol@gnosis.io>
2021-04-08 09:37:40 -0700

    Extracting common functions and data into testHelpers

---
+++ test/testHelpers.ts
@@ -0,0 +1,25 @@
+import { BigNumber } from "ethers";
+import { ethers } from "hardhat";
+
+import { OrderKind } from "../src/ts";
+
+export function fillBytes(count: number, byte: number): string {
+  return ethers.utils.hexlify([...Array(count)].map(() => byte));
+}
+
+export function fillUint(bits: number, byte: number): BigNumber {
+  return BigNumber.from(fillBytes(bits / 8, byte));
+}
+
+export const SAMPLE_ORDER = {
+  sellToken: fillBytes(20, 0x01),
+  buyToken: fillBytes(20, 0x02),
+  receiver: fillBytes(20, 0x03),
+  sellAmount: ethers.utils.parseEther("42"),
+  buyAmount: ethers.utils.parseEther("13.37"),
+  validTo: 0xffffffff,
+  appData: ethers.constants.HashZero,
+  feeAmount: ethers.utils.parseEther("1.0"),
+  kind: OrderKind.SELL,
+  partiallyFillable: false,
+};
