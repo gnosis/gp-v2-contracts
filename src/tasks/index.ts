24ddfaa8f685b9920a15b4179e92d6fcfb96372a
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2021-04-17 00:55:40 +0200

    Add task decoding settlement calldata

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,9 +1,11 @@
 import { setupCopyArtifactsTask } from "./artifacts";
+import { setupDecodeTask } from "./decode";
 import { setupSolversTask } from "./solvers";
 import { setupTenderlyTask } from "./tenderly";
 
 export function setupTasks(): void {
   setupCopyArtifactsTask();
+  setupDecodeTask();
   setupSolversTask();
   setupTenderlyTask();
 }
