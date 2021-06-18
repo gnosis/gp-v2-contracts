1397551fadfa15b3b3dcf5be8ea6b5c9acf3207e
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2021-06-18 12:24:43 +0200

    Update test/e2e/offchainAllowances.test.ts
    Co-authored-by: Federico Giacon <58218759+fedgiac@users.noreply.github.com>

--- test/e2e/offchainAllowances.test.ts
+++ test/e2e/offchainAllowances.test.ts
@@ -154,7 +154,7 @@ describe("E2E: Off-chain Allowances", () => {
 
   describe("Vault Allowance", () => {
     it("allows setting Vault relayer approval with interactions", async () => {
-      // Settle a trivial where all € stable coins trade 1:1.
+      // Settle a trivial trade where all € stable coins trade 1:1.
 
       const encoder = new SettlementEncoder(domainSeparator);
 
