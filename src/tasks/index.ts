f9d1adc0da566e9705af7a192935ac3ed0faebc4
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2021-06-21 17:08:53 +0000

    Port withdraw script (#702)

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,9 +1,11 @@
 import { setupCopyArtifactsTask } from "./artifacts";
 import { setupSolversTask } from "./solvers";
 import { setupTenderlyTask } from "./tenderly";
+import { setupWithdrawTask } from "./withdraw";
 
 export function setupTasks(): void {
   setupCopyArtifactsTask();
   setupSolversTask();
   setupTenderlyTask();
+  setupWithdrawTask();
 }
