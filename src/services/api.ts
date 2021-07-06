93cf140563804dde6034a0e3015f85ad0e77858c
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2021-07-06 16:57:46 +0200

    Standalone API functions

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,120 @@ import fetch, { RequestInit } from "node-fetch";
 
 import { Order, OrderKind, Signature } from "../ts";
 
+interface ApiCall {
+  network: string;
+}
+
+interface GetFeeQuery {
+  sellToken: string;
+  buyToken: string;
+  kind: OrderKind;
+  amount: BigNumber;
+}
+interface EstimateTradeAmountQuery {
+  sellToken: string;
+  buyToken: string;
+  kind: OrderKind;
+  amount: BigNumber;
+}
+interface PlaceOrderQuery {
+  order: Order;
+  signature: Signature;
+}
+interface GetExecutedSellAmountQuery {
+  uid: string;
+}
+
+interface OrderDetailResponse {
+  // Other fields are omitted until needed
+  executedSellAmount: string;
+}
+interface GetFeeResponse {
+  amount: string;
+  expirationDate: Date;
+}
+interface EstimateAmountResponse {
+  amount: string;
+  token: string;
+}
+
+async function call<T>(
+  route: string,
+  network: string,
+  init?: RequestInit,
+): Promise<T> {
+  const url = `https://protocol-${network}.dev.gnosisdev.com/api/v1/${route}`;
+  const response = await fetch(url, init);
+  const body = await response.text();
+  if (!response.ok) {
+    throw `Calling "${url} ${JSON.stringify(init)} failed with ${
+      response.status
+    }: ${body}`;
+  }
+  return JSON.parse(body);
+}
+
+export async function getFee({
+  sellToken,
+  buyToken,
+  kind,
+  amount,
+  network,
+}: GetFeeQuery & ApiCall): Promise<BigNumber> {
+  const response: GetFeeResponse = await call(
+    `fee?sellToken=${sellToken}&buyToken=${buyToken}&amount=${amount}&kind=${kind}`,
+    network,
+  );
+  return BigNumber.from(response.amount);
+}
+
+export async function estimateTradeAmount({
+  network,
+  sellToken,
+  buyToken,
+  kind,
+  amount,
+}: EstimateTradeAmountQuery & ApiCall): Promise<BigNumber> {
+  const response: EstimateAmountResponse = await call(
+    `markets/${sellToken}-${buyToken}/${kind}/${amount}`,
+    network,
+  );
+  return BigNumber.from(response.amount);
+}
+
+export async function placeOrder({
+  order,
+  signature,
+  network,
+}: PlaceOrderQuery & ApiCall): Promise<string> {
+  return await call("orders", network, {
+    method: "post",
+    body: JSON.stringify({
+      sellToken: order.sellToken,
+      buyToken: order.buyToken,
+      sellAmount: order.sellAmount.toString(),
+      buyAmount: order.buyAmount.toString(),
+      validTo: order.validTo,
+      appData: order.appData,
+      feeAmount: order.feeAmount.toString(),
+      kind: order.kind,
+      partiallyFillable: order.partiallyFillable,
+      signature: signature.data,
+      signingScheme: signature.scheme,
+      receiver: order.receiver,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export async function getExecutedSellAmount({
+  uid,
+  network,
+}: GetExecutedSellAmountQuery & ApiCall): Promise<BigNumber> {
+  const response: OrderDetailResponse = await call(`orders/${uid}`, network);
+  return BigNumber.from(response.executedSellAmount);
+}
+
 export class Api {
   network: string;
 
@@ -10,80 +124,20 @@ export class Api {
     this.network = network;
   }
 
-  private async call<T>(route: string, init?: RequestInit): Promise<T> {
-    const url = `https://protocol-${this.network}.dev.gnosisdev.com/api/v1/${route}`;
-    const response = await fetch(url, init);
-    const body = await response.text();
-    if (!response.ok) {
-      throw `Calling "${url} ${JSON.stringify(init)} failed with ${
-        response.status
-      }: ${body}`;
-    }
-    return JSON.parse(body);
+  async getFee(query: GetFeeQuery): Promise<BigNumber> {
+    return getFee({ ...query, network: this.network });
   }
-
-  async getFee(
-    selToken: string,
-    buyToken: string,
-    amount: BigNumber,
-    kind: OrderKind,
-  ): Promise<BigNumber> {
-    const response: GetFeeResponse = await this.call(
-      `fee?sellToken=${selToken}&buyToken=${buyToken}&amount=${amount}&kind=${kind}`,
-    );
-    return BigNumber.from(response.amount);
-  }
-
   async estimateTradeAmount(
-    selToken: string,
-    buyToken: string,
-    amount: BigNumber,
-    kind: OrderKind,
+    query: EstimateTradeAmountQuery,
   ): Promise<BigNumber> {
-    const response: EstimateAmountResponse = await this.call(
-      `markets/${selToken}-${buyToken}/${kind}/${amount}`,
-    );
-    return BigNumber.from(response.amount);
+    return estimateTradeAmount({ ...query, network: this.network });
   }
-
-  async placeOrder(order: Order, signature: Signature): Promise<string> {
-    return await this.call("orders", {
-      method: "post",
-      body: JSON.stringify({
-        sellToken: order.sellToken,
-        buyToken: order.buyToken,
-        sellAmount: order.sellAmount.toString(),
-        buyAmount: order.buyAmount.toString(),
-        validTo: order.validTo,
-        appData: order.appData,
-        feeAmount: order.feeAmount.toString(),
-        kind: order.kind,
-        partiallyFillable: order.partiallyFillable,
-        signature: signature.data,
-        signingScheme: signature.scheme,
-        receiver: order.receiver,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
+  async placeOrder(query: PlaceOrderQuery): Promise<string> {
+    return placeOrder({ ...query, network: this.network });
   }
-
-  async getExecutedSellAmount(uid: string): Promise<BigNumber> {
-    const response: OrderDetailResponse = await this.call(`orders/${uid}`);
-    return BigNumber.from(response.executedSellAmount);
+  async getExecutedSellAmount(
+    query: GetExecutedSellAmountQuery,
+  ): Promise<BigNumber> {
+    return getExecutedSellAmount({ ...query, network: this.network });
   }
-}
-
-interface OrderDetailResponse {
-  // Other fields are omitted until needed
-  executedSellAmount: string;
-}
-
-interface GetFeeResponse {
-  amount: string;
-  expirationDate: Date;
-}
-
-interface EstimateAmountResponse {
-  amount: string;
-  token: string;
 }
