0f3d00d7f40471fb120cabf65d14ee5b1bbfd6e6
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2021-07-09 15:24:40 +0200

    Add script to dump tokens

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,5 +1,6 @@
 import { setupCopyArtifactsTask } from "./artifacts";
 import { setupDecodeTask } from "./decode";
+import { setupDumpTask } from "./dump";
 import { setupSolversTask } from "./solvers";
 import { setupTenderlyTask } from "./tenderly";
 import { setupWithdrawTask } from "./withdraw";
@@ -7,6 +8,7 @@ import { setupWithdrawTask } from "./withdraw";
 export function setupTasks(): void {
   setupCopyArtifactsTask();
   setupDecodeTask();
+  setupDumpTask();
   setupSolversTask();
   setupTenderlyTask();
   setupWithdrawTask();
