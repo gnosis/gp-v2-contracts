38fa3d1af0d96c7025f3abda84f094b2f08811bb
Federico Giacon <58218759+fedgiac@users.noreply.github.com>
2021-08-09 17:42:46 +0200

    Ignore coverage folder when linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,13 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
-  ignorePatterns: ["build/", "node_modules/", "!.prettierrc.js", "lib/"],
+  ignorePatterns: [
+    "build/",
+    "node_modules/",
+    "coverage/",
+    "!.prettierrc.js",
+    "lib/",
+  ],
   rules: {
     "import/order": [
       "error",
