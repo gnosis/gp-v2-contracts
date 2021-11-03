5bbf09c7928c193e8fae62463c3bfe2090b49ee1
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2021-11-03 16:28:45 +0100

    Add more context to simulation

--- src/contracts/reader/GPv2TradeSimulator.sol
+++ src/contracts/reader/GPv2TradeSimulator.sol
@@ -26,15 +26,37 @@ contract GPv2TradeSimulator {
     /// @dev Initial token state used internally for computing balance changes.
     struct TokenState {
         IERC20 token;
+        address account;
         uint256 initialBalance;
+    }
+
+    /// @dev Account state for tracking individual token states per user.
+    struct AccountState {
+        TokenState sellTokenState;
+        TokenState buyTokenState;
     }
 
     /// @dev Simulation context.
     struct Context {
+        uint256 gasCounter;
         GPv2Transfer.Data[] inTransfers;
         GPv2Transfer.Data[] outTransfers;
-        TokenState sellTokenState;
-        TokenState buyTokenState;
+        AccountState contractAccount;
+        AccountState ownerAccount;
+    }
+
+    /// @dev Address balance changes included in the results.
+    struct BalanceDelta {
+        int256 sellTokenDelta;
+        int256 buyTokenDelta;
+    }
+
+    /// @dev Simulation result.
+    struct Result {
+        uint256 gasUsed;
+        uint256 executedBuyAmount;
+        BalanceDelta contractBalance;
+        BalanceDelta ownerBalance;
     }
 
     /// @dev Sentinal value to indicate that the buy amount for the trade's out
@@ -51,7 +73,7 @@ contract GPv2TradeSimulator {
     /// One notable difference, is the simulation doesn't actually require an
     /// order. This makes using this method inaccurate for predicting gas usage
     /// for specific trades. However, seeing as computing trade executions is
-    /// completely independant of external contracts (i.e. it does not depend on
+    /// completely independent of external contracts (i.e. it does not depend on
     /// the tokens being traded or the interactions being executed) the gas
     /// consumption should be off by a predicatable amount.
     ///
@@ -60,9 +82,9 @@ contract GPv2TradeSimulator {
     function simulateTrade(
         Trade calldata trade,
         GPv2Interaction.Data[] calldata interactions
-    ) external returns (int256 sellDelta, int256 buyDelta) {
-        GPv2Settlement self = GPv2Settlement(payable(address(this)));
+    ) external returns (Result memory result) {
         Context memory context = createContext(trade);
+        GPv2Settlement self = GPv2Settlement(payable(address(this)));
 
         self.vaultRelayer().transferFromAccounts(context.inTransfers);
 
@@ -73,8 +95,7 @@ contract GPv2TradeSimulator {
         updateOutTransferAmount(context);
         self.vault().transferToAccounts(context.outTransfers);
 
-        sellDelta = computeBalanceDelta(context.sellTokenState);
-        buyDelta = computeBalanceDelta(context.buyTokenState);
+        finalizeResult(context, result);
     }
 
     /// @dev Initializes a simulation context in memory for the current trade
@@ -87,30 +108,31 @@ contract GPv2TradeSimulator {
         view
         returns (Context memory context)
     {
+        context.gasCounter = gasCounter();
+
         context.inTransfers = new GPv2Transfer.Data[](1);
-        context.inTransfers[0].account = trade.owner;
-        context.inTransfers[0].token = trade.sellToken;
-        context.inTransfers[0].amount = trade.sellAmount;
-        context.inTransfers[0].balance = trade.sellTokenBalance;
+        {
+            GPv2Transfer.Data memory inTransfer = context.inTransfers[0];
+            inTransfer.account = trade.owner;
+            inTransfer.token = trade.sellToken;
+            inTransfer.amount = trade.sellAmount;
+            inTransfer.balance = trade.sellTokenBalance;
+        }
 
         context.outTransfers = new GPv2Transfer.Data[](1);
-        context.outTransfers[0].account = trade.receiver ==
-            GPv2Order.RECEIVER_SAME_AS_OWNER
-            ? trade.owner
-            : trade.receiver;
-        context.outTransfers[0].token = trade.buyToken;
-        context.outTransfers[0].amount = trade.buyAmount;
-        context.outTransfers[0].balance = trade.buyTokenBalance;
+        {
+            GPv2Transfer.Data memory outTransfer = context.outTransfers[0];
+            outTransfer.account = trade.receiver ==
+                GPv2Order.RECEIVER_SAME_AS_OWNER
+                ? trade.owner
+                : trade.receiver;
+            outTransfer.token = trade.buyToken;
+            outTransfer.amount = trade.buyAmount;
+            outTransfer.balance = trade.buyTokenBalance;
+        }
 
-        context.sellTokenState.token = trade.sellToken;
-        context.sellTokenState.initialBalance = trade.sellToken.balanceOf(
-            address(this)
-        );
-
-        context.buyTokenState.token = trade.buyToken;
-        context.buyTokenState.initialBalance = trade.buyToken.balanceOf(
-            address(this)
-        );
+        initializeAccountState(context.contractAccount, address(this), trade);
+        initializeAccountState(context.ownerAccount, trade.owner, trade);
     }
 
     /// @dev Updates the out transfer token amount to be the exact amount of buy
@@ -119,17 +141,68 @@ contract GPv2TradeSimulator {
     function updateOutTransferAmount(Context memory context) private view {
         if (context.outTransfers[0].amount == USE_ALL_RECEIVED_BUY_TOKENS) {
             context.outTransfers[0].amount ==
-                computeBalanceDelta(context.buyTokenState).toUint256();
+                computeTokenDelta(context.contractAccount.buyTokenState)
+                    .toUint256();
         }
     }
 
-    /// @dev Compues the balance delta for the specified token state.
-    function computeBalanceDelta(TokenState memory state)
+    /// @dev Computes the simulation result for the given context.
+    function finalizeResult(Context memory context, Result memory result)
+        private
+        view
+    {
+        result.executedBuyAmount = context.outTransfers[0].amount;
+        computeBalanceDelta(context.contractAccount, result.contractBalance);
+        computeBalanceDelta(context.ownerAccount, result.ownerBalance);
+        result.gasUsed = context.gasCounter - gasCounter();
+    }
+
+    /// @dev Initializes an account state for the specified address.
+    function initializeAccountState(
+        AccountState memory state,
+        address account,
+        Trade calldata trade
+    ) private view {
+        initializeTokenState(state.sellTokenState, trade.sellToken, account);
+        initializeTokenState(state.buyTokenState, trade.buyToken, account);
+    }
+
+    /// @dev Initializes a token state for the specified token and address
+    function initializeTokenState(
+        TokenState memory state,
+        IERC20 token,
+        address account
+    ) private view {
+        state.token = token;
+        state.account = account;
+        state.initialBalance = token.balanceOf(account);
+    }
+
+    /// @dev Computes the account's balance delta for all tokens.
+    function computeBalanceDelta(
+        AccountState memory state,
+        BalanceDelta memory result
+    ) private view {
+        result.sellTokenDelta = computeTokenDelta(state.sellTokenState);
+        result.buyTokenDelta = computeTokenDelta(state.buyTokenState);
+    }
+
+    /// @dev Computes the token balance delta for the specified token state.
+    function computeTokenDelta(TokenState memory state)
         private
         view
         returns (int256)
     {
-        uint256 currentBalance = state.token.balanceOf(address(this));
+        uint256 currentBalance = state.token.balanceOf(state.account);
         return currentBalance.toInt256() - state.initialBalance.toInt256();
+    }
+
+    /// @dev Reads the remaining gas counter.
+    ///
+    /// This function abstracts over the EVM `gas()` assembly instruction.
+    function gasCounter() private view returns (uint256 g) {
+        assembly {
+            g := gas()
+        }
     }
 }
